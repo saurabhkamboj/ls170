0594bdb7b3e32acbd2d4670f6804b11e93ac0bbe
Saurabh Kamboj <skamboj97@gmail.com>
2024-01-30 18:44:42 +0530

    Modify file

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -33,4 +33,4 @@ function server () {
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+netcat -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
