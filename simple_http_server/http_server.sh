523b34ef9554933295607c1e3b1afe0aff3c90b3
EC2 Default User <ec2-user@ip-172-31-43-34.ec2.internal>
2024-01-30 13:06:15 +0000

    Modify file

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -3,17 +3,30 @@
 function server () {
   while true
   do
-    read method path version
-    if [[ $method = 'GET' ]]; then
-
-      if [[ -f ./www/$path ]]; then
-
-        echo -e 'HTTP/1.1 200 OK\r\n\r\n'; cat ./www/$path
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    if [[ $method = 'GET' ]]
+    then
+      if [[ -f ./www/$path ]]
+      then
+        echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $(wc -c <'./www/'$path)}\r\n\r\n"
+        cat ./www/$path
       else
-        echo -e 'HTTP/1.1 404 Not Found\r\n\r\n'
+        echo -e 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n'
       fi
     else
-      echo -e 'HTTP/1.1 400 Bad Request\r\n\r\n'
+      echo -e 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n'
     fi
   done
 }
